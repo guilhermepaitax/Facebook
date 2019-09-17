f2e8f2a235b2a22d9731f19f5755fdf713318aa7
Guilherme Paitax <guilhermepaitax16@gmail.com>
2019-09-17 11:19:35 -0300

    Criando a lista de posts

---
+++ src/components/PostList.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+import '../styles/PostList.css';
+import Post from './Post';
+
+export default class PostList extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        author: {
+          name: 'Julio Alcantara',
+          avatar: 'http://url-da-imagem.com/imagem.jpg'
+        },
+        date: '04 Jun 2019',
+        content: 'Pessoal, alguém sabe se a Rocketseat está contratando?',
+        comments: [
+          {
+            id: 1,
+            author: {
+              name: 'Diego Fernandes',
+              avatar: 'http://url-da-imagem.com/imagem.jpg'
+            },
+            content: "Conteúdo do comentário"
+          }
+        ],
+      },
+      {
+        id: 2,
+        // Restante dos dados de um novo post
+      }
+    ]
+  };
+
+  render() {
+    return (
+      <div className="feed">
+        <div className="feed-body">
+          <Post />
+          <Post />
+          <Post />
+        </div>
+      </div>
+    );
+  }
+}
